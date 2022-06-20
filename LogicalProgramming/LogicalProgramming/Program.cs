31a7e0fa704eeac77743f0acf4b538a301583246
akshayb407 <akshayb407@gmail.com>
2022-06-20 15:39:48 +0530

    all programs added

---
+++ LogicalProgramming/LogicalProgramming/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LogicalProgramming
+{
+    internal class Program
+
+    {
+        public static void Main()
+        {
+
+            int n = 10, firstTerm = 0, secondTerm = 1; 
+            Console.WriteLine("Fibonacci Series till " + n + " terms:");
+
+            for (int i = 1; i <= n; ++i)
+            {
+                Console.WriteLine(firstTerm + ", ");
+
+
+                int nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+                
+
+
+
+            }
+        }
+    }
+}
